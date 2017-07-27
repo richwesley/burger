80e565c8514da1f269555a0752bfc6c7bf708e8e
Rich Cross <richwesley@comcast.net>
2017-07-27 16:06:13 -0700

    port

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,9 @@ var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else {
+} else {
   connection = mysql.createConnection({
+    port: "3306",
     host: "localhost",
     user: "root",
     password: "",
