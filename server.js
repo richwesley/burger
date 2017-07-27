11bfdb502f36af0477f34e5be9ebdbee40001362
Rich Cross <richwesley@comcast.net>
2017-07-27 10:48:38 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const path = require("path");
+const app = express();
+const exphbs = require("express-handlebars");
+
+const port = 3002 || process.env.PORT;
+
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
