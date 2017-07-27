113f9775ca5bde8cd20b37b2e39d8a9e2ea33284
Rich Cross <richwesley@comcast.net>
2017-07-27 12:41:27 -0700

    more fix

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWS_URL) {
+if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSSDB_URL);
 }else {
   connection = mysql.createConnection({
