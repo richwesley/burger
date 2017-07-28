1a41591a64f9cbea38523589553f0c6391aeb4d2
Rich Cross <richwesley@comcast.net>
2017-07-27 18:13:10 -0700

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const exphbs = require("express-handlebars");
 
-const port = 3002 || process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.use(express.static("public"));
 
